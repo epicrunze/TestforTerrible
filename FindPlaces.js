9a9be50c7b0b6be0cb261ffc23c12f8d0dbcbf94
epicrunze <42049670+epicrunze@users.noreply.github.com>
2019-03-10 13:39:41 -0400

    Findplaces.js

---
+++ FindPlaces.js
@@ -0,0 +1,31 @@
+var map;
+var serviceMemes;
+var LocationsMemes;
+
+var LatMidpointMemes = '43.4643';
+var LongMidpointMemes = '80.5204';
+var RadiusMemes = '1000';
+
+function FindPlaces() {
+var originMemes = new google.maps.LatLng(LatMidpointMemes, LongMidpointMemes);
+
+serviceMemes = new google.maps.places.PlacesService(map);
+
+	var request = {
+		location: originMemes,
+		radius: RadiusMemes,
+		RankBy: 'google.maps.places.RankBy.DISTANCE',
+	}
+
+serviceMemes.nearbySearch(request, callback);
+
+
+}
+
+function callback(results, status) {
+	if (status == google.maps.places.PlacesServiceStatus.OK) {
+		for (var i = 0; i < results.length; i++) {
+			LocationsMemes.push(results[i]);
+		}
+	}
+}
