0b6386fa7ad145717d0f54759e75e4264c152f1d
epicrunze <42049670+epicrunze@users.noreply.github.com>
2019-03-10 18:04:18 -0400

    Update FindPlaces.js

--- FindPlaces.js
+++ FindPlaces.js
@@ -6,6 +6,18 @@ var LatMidpointMemes = 40;
 var LongMidpointMemes = -80;
 
 
+function callmemes(results, status) {
+	if (status == google.maps.places.PlacesServiceStatus.OK) {
+			LocationsMemes = results;
+	}
+}
+function wait(ms)
+{
+var d = new Date();
+var d2 = null;
+do { d2 = new Date(); }
+while(d2-d < ms);
+}
 
 function FindPlaces() {
 
@@ -17,11 +29,11 @@ map = new google.maps.Map(document.getElementById('map'), {
 
 serviceMemes = new google.maps.places.PlacesService(map);
 
-	var memequest = {
-		location: {lat: LatMidpointMemes, lng: LongMidpointMemes},
-		type: ['accounting', 'airport', 'amusement_park', 'aquarium', 'art_gallery', 'bakery', 'bank', 'bar', 'beauty_salon', 'bicycle_store', 'book_store', 'bowling_alley', 'bus_station', 'cafe', 'campground', 'car_dealer', 'car_rental', 'car_repair', 'car_wash', 'casino', 'cemetery', 'church', 'city_hall', 'clothing_store', 'convenience_store', 'courthouse', 'dentist', 'department_store', 'doctor', 'electrician', 'electronics_store', 'embassy', 'fire_station', 'florist', 'funeral_home', 'furniture_store', 'gas_station', 'gym', 'hair_care', 'hardware_store', 'hindu_temple', 'home_goods_store', 'hospital', 'insurance_agency', 'jewelry_store', 'laundry', 'lawyer', 'library', 'liquor_store', 'local_government_office', 'locksmith', 'lodging', 'meal_delivery', 'meal_takeaway', 'mosque', 'movie_rental', 'movie_theater', 'moving_company', 'museum', 'night_club', 'painter', 'park', 'parking', 'pet_store', 'pharmacy', 'physiotherapist', 'plumber', 'police', 'post_office', 'real_estate_agency', 'restaurant', 'roofing_contractor', 'rv_park', 'school', 'shoe_store', 'shopping_mall', 'spa', 'stadium', 'storage', 'store', 'subway_station', 'supermarket', 'synagogue', 'taxi_stand', 'train_station', 'transit_station', 'travel_agency', 'veterinary_care', 'zoo'],
-		rankBy: google.maps.places.RankBy.DISTANCE
-	}
+var memequest = {
+	location: {lat: LatMidpointMemes, lng: LongMidpointMemes},
+	type: ['accounting', 'airport', 'amusement_park', 'aquarium', 'art_gallery', 'bakery', 'bank', 'bar', 'beauty_salon', 'bicycle_store', 'book_store', 'bowling_alley', 'bus_station', 'cafe', 'campground', 'car_dealer', 'car_rental', 'car_repair', 'car_wash', 'casino', 'cemetery', 'church', 'city_hall', 'clothing_store', 'convenience_store', 'courthouse', 'dentist', 'department_store', 'doctor', 'electrician', 'electronics_store', 'embassy', 'fire_station', 'florist', 'funeral_home', 'furniture_store', 'gas_station', 'gym', 'hair_care', 'hardware_store', 'hindu_temple', 'home_goods_store', 'hospital', 'insurance_agency', 'jewelry_store', 'laundry', 'lawyer', 'library', 'liquor_store', 'local_government_office', 'locksmith', 'lodging', 'meal_delivery', 'meal_takeaway', 'mosque', 'movie_rental', 'movie_theater', 'moving_company', 'museum', 'night_club', 'painter', 'park', 'parking', 'pet_store', 'pharmacy', 'physiotherapist', 'plumber', 'police', 'post_office', 'real_estate_agency', 'restaurant', 'roofing_contractor', 'rv_park', 'school', 'shoe_store', 'shopping_mall', 'spa', 'stadium', 'storage', 'store', 'subway_station', 'supermarket', 'synagogue', 'taxi_stand', 'train_station', 'transit_station', 'travel_agency', 'veterinary_care', 'zoo'],
+	rankBy: google.maps.places.RankBy.DISTANCE
+}
 
 serviceMemes.nearbySearch(memequest, callmemes);
 wait(100)
@@ -38,15 +50,3 @@ wait(100)
 return NamesAndRatings;
 }
 
-function callmemes(results, status) {
-	if (status == google.maps.places.PlacesServiceStatus.OK) {
-			LocationsMemes = results;
-	}
-}
-function wait(ms)
-{
-var d = new Date();
-var d2 = null;
-do { d2 = new Date(); }
-while(d2-d < ms);
-}
